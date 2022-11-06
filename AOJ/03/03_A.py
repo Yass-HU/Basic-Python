f8a7c537e76652e8db37d9d4621fce11ca8b3731
Yass Fukushima <y.fukushima@4nonome.com>
2022-11-06 15:44:36 +0900

    fixed_03_C

--- AOJ/03/03_A.py
+++ AOJ/03/03_A.py
@@ -1,10 +1,3 @@
-#Write a program which prints 1000 "Hello World".
-
-#Input
-#There is no input for this problem.
-
-#Output
-#The output consists of 1000 lines. Print "Hello World" in each line.
 
 for t in range(1000):
     print("Hello World")
