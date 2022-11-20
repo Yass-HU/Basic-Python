f61457167b1093c69266beff1695aeed89cedd3f
Yass Fukushima <y.fukushima@4nonome.com>
2022-11-20 13:30:42 +0900

    remove memo

--- AOJ/04/04_A.py
+++ AOJ/04/04_A.py
@@ -2,8 +2,4 @@ a, b = map(int,input().split())
 d = a // b
 r = a % b
 f = a / b
-#print(f"a is {a:.3f}")
 print(f"{d} {r} {f:.5f}")
-#print(d,r,f)
-#print('{1},{2},{:.5f}'.format(d, r, f))#-5f{0:.5f}
-#error result check fについては0.00001以下の誤差があってもよいものとします？5keta
