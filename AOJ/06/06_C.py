98b985a646d134914efec6bb16fce316b4db86a5
Yass_Fukushima <63622006+angie-web@users.noreply.github.com>
2022-12-21 09:57:35 +0900

    Aoj6 1220  (#8)
    * 1st commit
    
    * remove memo
    
    * first commit
    
    * remove memo
    
    * my first commit

---
+++ AOJ/06/06_C.py
@@ -0,0 +1,12 @@
+count = [[[0 for i in range(10)] for j in range(3)] for k in range(4)]
+n = int(input())
+for x in range(n):
+    b,f,r,v = (map(int, input().split()))
+    count[b-1][f-1][r-1]+=v
+for i in range(4):
+    for j in range(3):
+        for k in range(10):
+            print(f' {count[i][j][k]}', end="")
+        print()
+    if i ==3:break
+    print('#####################')
